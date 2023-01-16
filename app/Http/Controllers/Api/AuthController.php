266df1c2b29733dd804bb7d4b83483b31723daca
Inna <isoyan.inna@gmail.com>
2023-01-16 18:45:59 +0400

    API Auth Sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function createUser(Request $request)
+    {
+
+        $data = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => ['required', 'emial', 'unique:users,email'],
+            'role_id' => ['required'],
+            'password' => 'required',
+        ]);
+
+        try {
+            $user = User::query()->create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'role_id' => $request->role,
+                'password' => Hash::make($request->password),
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function loginUser(Request $request){
+
+        $data = Validator::make($request->all(),[
+           'email' => ['required', 'email'],
+            'password' => 'required',
+        ]);
+
+
+        if(!Auth::attempt($request->only(['email', 'password']))){
+            return response()->json([
+                'status' => false,
+                'message' => 'Email or Password does not match with our record.',
+            ], 401);
+        }
+
+        $user = User::query()->where('email',$request->email)->first();
+
+        return response()->json([
+           'status' => true,
+           'message' => 'Logged in successfully',
+            'token' => $user->createToken("API TOKEN")->plainTextToken], 200
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('products', ProductController::class);
+Route::apiResource('products', ProductController::class)->middleware('auth:sanctum');
+Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
 
-Route::apiResource('post', PostController::class);
+Route::post('auth/register', [AuthController::class, 'createUser']);
+Route::post('auth/login', [AuthController::class, 'loginUser']);
+
